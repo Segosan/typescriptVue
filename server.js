702768886c605b7501c9b2284268b77e80fe5937
Victor <vsegsan@gmail.com>
2021-10-14 02:20:24 +0100

    he

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(function(req,res,next){
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next();
 })
-app.use(express.static(path.join(__dirname, '../app/dist')));
+app.use(express.static(__dirname, '../app/dist'));
 
 app.use('/api',apiRoutes);
 
